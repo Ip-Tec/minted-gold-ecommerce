cded2102b47ba75f2c40becc59c49882da05e4e3
ip-tec <otakhorpeter@gmail.com>
2023-12-05 00:32:59 +0100

    made some change to user and admin

--- minted-gold-admin/pages/api/auth/[...nextauth].js
+++ minted-gold-admin/pages/api/auth/[...nextauth].js
@@ -14,6 +14,7 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
+      timeout: 900000,
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
